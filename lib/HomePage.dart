240f3028c7a48c0d5596aa5672cc02f3ae6ee343
namanagarwal94 <naman.agarwal1_cs19@gla.ac.in>
2021-05-31 21:49:23 +0530

    method for controlling y axis with gravity variance and jump and start game method.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -11,6 +11,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool gameHasStarted = false;
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  void startGame() {
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      gameHasStarted = true;
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
+      setState(() {
+        birdYaxis = initialHeight - height;
+      });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
